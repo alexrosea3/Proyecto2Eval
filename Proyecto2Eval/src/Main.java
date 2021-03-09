d8bcc62dfe77c73f67d27db313ec6244aa95dbb8
i1i6 <i1i6@info2008-245.sanjose.local>
2021-03-09 11:40:03 +0100

    Actualizacion ventana

---
+++ Proyecto2Eval/src/Main.java
@@ -0,0 +1,10 @@
+import Vista.Vista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vista ventana = new Vista();
+	}
+
+}
--- Proyecto2Eval/src/Vista/Vista.java
+++ Proyecto2Eval/src/Vista/Vista.java
@@ -1,5 +1,30 @@
 package Vista;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class Vista {
 
+	JTextField cajatext = new JTextField(30);
+	ArrayList<JButton> btn = new ArrayList();
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+
+	public Vista() {
+
+		for (int i = 0; i < 3; i++) {
+			panel.add(new JButton());
+		}
+		panel.add(cajatext);
+		frame.setSize(400, 400);
+
+		frame.add(panel);
+
+		frame.setVisible(true);
+
+	}
 }
